159500fd4742e5d361baafa624ab9555a5110e1c
dp4wtea <81384821+dp4wtea@users.noreply.github.com>
2023-07-18 11:19:14 +0200

    Initial commit

---
+++ src/main/kotlin/com/lvishe/phontynyasy/PhontynyasyApplication.kt
@@ -0,0 +1,11 @@
+package com.lvishe.phontynyasy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhontynyasyApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhontynyasyApplication>(*args)
+}
---
+++ src/test/kotlin/com/lvishe/phontynyasy/PhontynyasyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lvishe.phontynyasy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhontynyasyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
